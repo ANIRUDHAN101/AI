7f4ba9d70e910b6013f9b321115c52c026075fc1
Anirudhan k s <86823699+ANIRUDHAN101@users.noreply.github.com>
2023-01-17 14:00:36 +0530

    Add files via upload

--- g_astar.py
+++ g_astar.py
@@ -8,46 +8,48 @@ import gym
 @dataclass(order=True)
 class PrioritizedItem:
     priority: int
-    item: Any=field(compare=False)
-    patent: Any=field(compare=False)
+    item: Any = field(compare=False)
+    patent: Any = field(compare=False)
 
-class Dijkstra:
-    def __init__(self, p=(0,0), f=(10,10) , g=None):
+
+class A_star:
+    def __init__(self, p=(0, 0), f=(10, 10), g=None):
         self.Ground = []
         self.P = p
         self.F = f
         if g == None:
             self.g = ["SFFHHHHHHHH",
-                "FFFFFFFFFFF",
-                "HFFHFHFFFHF",
-                "HFFFFFFFFHF",
-                "HFFHHHHFHFF",
-                "HFFFFFHFFHF",
-                "HHHFHHHFFHF",
-                "HFHFFFFHFFH",
-                "HFFFHHHFFFF",
-                "HFHFFFFFHFF",
-                "HHHHHHHHHFG"]
+                      "FFFFFFFFFFF",
+                      "HFFHFHFFFHF",
+                      "HFFFFFFFFHF",
+                      "HFFHHHHFHFF",
+                      "HFFFFFHFFHF",
+                      "HHHFHHHFFHF",
+                      "HFHFFFFHFFH",
+                      "HFFFHHHFFFF",
+                      "HFHFFFFFHFF",
+                      "HHHHHHHHHFG"]
         else:
             self.g = g
 
         # Create the custom self.environment
-        self.env = gym.make("FrozenLake-v1",desc=self.g,
-        render_mode="human", is_slippery=False)
+        self.env = gym.make("FrozenLake-v1", desc=self.g,
+                            render_mode="human", is_slippery=False)
         self.env.action_space.seed(42)
         observation, info = self.env.reset(seed=42)
         self.Size = (len(self.g), len(self.g[0]))
         for r in self.g:
-           self.Ground.append(list(r))
-
-
-
+            self.Ground.append(list(r))
 
         # Reset the self.environment and start the game
         obs = self.env.reset()
+    # computes the manhaten distance between Goal and current node
 
-    def push_itm(self,Open,itm):
-        #print(Open)
+    def manhaten(self, x0, x1):
+        return abs(x0[0]-x1[0]) + abs(x0[1]-x1[1])
+
+    def push_itm(self, Open, itm):
+        # print(Open)
         for a in Open:
             # print(a)
             if a.item == itm.item:
@@ -57,13 +59,14 @@ class Dijkstra:
                 return
         heapq.heappush(Open, itm)
         return
+
     def run(self):
         path = 'F'
         goal = 'G'
         self.Closed = {}
         Open = []
 
-        itm = PrioritizedItem(0,(int(self.P[0]), int(self.P[1])),None)
+        itm = PrioritizedItem(0, (int(self.P[0]), int(self.P[1])), None)
         Open.append(itm)
         # end the loop if all possible node's are explored
         while len(Open) != 0:
@@ -71,7 +74,7 @@ class Dijkstra:
             self.Closed[str(vertex.item)] = vertex.patent
             dist = vertex.priority
             node = vertex.item
-            #if the Pacman fineds food break the loop
+            # if the Pacman fineds food break the loop
             if node == self.F:
                 print("goal!!")
                 self.Closed[self.F] = vertex.patent
@@ -81,37 +84,38 @@ class Dijkstra:
             UP, DOWN, LEFT, RIGHT = node[0]-1, node[0]+1, node[1]-1, node[1]+1
 
             # search for boundary and update nodes to Open
-            if UP>0:
-                if   self.Ground[UP][node[1]] == path or self.Ground[UP][node[1]] == goal :
-                    itm = PrioritizedItem(dist+1,(UP, node[1]),node)
+            if UP > 0:
+                if self.Ground[UP][node[1]] == path or self.Ground[UP][node[1]] == goal:
+                    dist = dist + 1 + self.manhaten(((UP, node[1])), self.F)
+                    itm = PrioritizedItem(dist, (UP, node[1]), node)
                     self.push_itm(Open, itm)
-                    
+
                     self.Ground[UP][node[1]] = '#'
 
-            if LEFT > 0 :
-                if self.Ground[node[0]][LEFT] == path or  self.Ground[node[0]][LEFT] == goal :
-                    
-                    itm = PrioritizedItem(dist+1,(node[0], LEFT),node)
+            if LEFT > 0:
+                if self.Ground[node[0]][LEFT] == path or self.Ground[node[0]][LEFT] == goal:
+                    dist = dist + 1 + self.manhaten((node[0], LEFT), self.F)
+                    itm = PrioritizedItem(dist, (node[0], LEFT), node)
                     self.push_itm(Open, itm)
-                    
+
                     self.Ground[node[0]][LEFT] = '#'
 
             if RIGHT < self.Size[1]:
-                if  self.Ground[node[0]][RIGHT] == path or self.Ground[node[0]][RIGHT] == goal:
-            
-                    itm = PrioritizedItem(dist+1,(node[0], RIGHT),node)
+                if self.Ground[node[0]][RIGHT] == path or self.Ground[node[0]][RIGHT] == goal:
+                    dist = dist + 1 + self.manhaten((node[0], RIGHT), self.F)
+                    itm = PrioritizedItem(dist, (node[0], RIGHT), node)
                     self.push_itm(Open, itm)
-                
-                    self.Ground[node[0]][RIGHT] = '#'
-            
-            if DOWN < self.Size[0]:
-                if  self.Ground[DOWN][node[1]] == path or self.Ground[DOWN][node[1]] == goal:
 
-                    itm = PrioritizedItem(dist+1,(DOWN, node[1]),node)
+                    self.Ground[node[0]][RIGHT] = '#'
+
+            if DOWN < self.Size[0]:
+                if self.Ground[DOWN][node[1]] == path or self.Ground[DOWN][node[1]] == goal:
+                    dist = dist + 1 + self.manhaten((DOWN, node[1]), self.F)
+                    itm = PrioritizedItem(dist, (DOWN, node[1]), node)
                     self.push_itm(Open, itm)
-                
+
                     self.Ground[DOWN][node[1]] = '#'
-              
+
     def play(self):
 
         s = self.F
@@ -122,35 +126,37 @@ class Dijkstra:
             s = self.Closed[str(s)]
 
         print(path)
-        for  i in reversed(range(1,len(path))):
+        for i in reversed(range(1, len(path))):
             p0 = path[i]
             p1 = path[i-1]
             # print(p0,p1)
             # print(p0[0]-p1[0],p0[1]-p1[1])
             # move down
-            if p0[0] - p1[0] <0:
+            if p0[0] - p1[0] < 0:
                 p = abs(p0[0] - p1[0])
                 # print(p)
                 for i in range(p):
                     self.env.step(1)
             # move up
-            elif p0[0] - p1[0] >0:
+            elif p0[0] - p1[0] > 0:
                 p = abs(p0[0] - p1[0])
                 for i in range(p):
                     self.env.step(3)
             # move left
-            if p0[1] - p1[1] >0:
+            if p0[1] - p1[1] > 0:
                 p = abs(p0[1] - p1[1])
                 for i in range(p):
                     self.env.step(0)
             # move right
-            elif  p0[1] - p1[1] <0:
+            elif p0[1] - p1[1] < 0:
                 p = abs(p0[1] - p1[1])
                 for i in range(p):
                     self.env.step(2)
 
         self.env.close()
+
+
 if __name__ == "__main__":
-    d = Dijkstra()
+    d = A_star()
     d.run()
     d.play()
